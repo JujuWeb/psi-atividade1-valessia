514e260dac1151d61029db132ae952c8b6717145
Valessia Juliany <valessia.juliany@escolar.ifrn.edu.br>
2026-09-04 14:30:22 -0300

    glow up
    04/09

--- psi-atividade1-valessia/app.py
+++ psi-atividade1-valessia/app.py
@@ -1,1 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+import models
+
+app = Flask(__name__)
+secret_key = "amarelo"
+
+@app.route('/')
+def index():
+    buscar_livros = models.buscar_livros()
+    return render_template('index.html', livros=buscar_livros)  
+
+@app.route('/livros')
+def livros():
+    buscar_livros = models.buscar_livros()
+    return render_template('livros.html', livros=buscar_livros)  
